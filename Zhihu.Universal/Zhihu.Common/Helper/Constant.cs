0ba7c5b16ab14c34969eab9237cf684c9156c23a
Zhang Cheng <13501393281@163.com>
2016-03-26 20:54:41 -0400

    just realize there is an error in the font size... changing it back

--- Zhihu.Universal/Zhihu.Common/Helper/Constant.cs
+++ Zhihu.Universal/Zhihu.Common/Helper/Constant.cs
@@ -7,7 +7,7 @@
             public const double PageTitle = 32.0;
             public const double FeedTitle = 19.0;
             public const double FeedVerb = 14.0;
-            public const double FeedSummary = 14.0;
+            public const double FeedSummary = 15.0;
             public const double VoteCount = 11.0;
         }
     }
